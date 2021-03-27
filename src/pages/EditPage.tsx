6d20241bd49d974b6d852499b336c6a3ce672daf
Ithamar R. Adema <ithamar@upgrade-android.com>
2021-03-27 09:51:02 +0100

    Add cursor movement on grid

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { Canvas } from 'react-three-fiber'
+import { Canvas, PointerEvent } from 'react-three-fiber'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { Object3D } from 'three';
 
 const gridSize = 1;
 const gridCount = 64;
 const levelSize = gridCount * gridSize;
 
 export default function EditPage() {
+    const cursorRef = React.useRef<Object3D>()
+
+    const moveCursor = (event: PointerEvent) => {
+        const { point } = event
+        event?.stopPropagation()
+
+        const gridX = Math.floor(point.x / gridSize)
+        const gridY = Math.floor(point.z / gridSize)
+        console.log(gridX, gridY)
+
+        if (cursorRef.current) {
+            cursorRef.current.position.set(
+                gridX * gridSize + gridSize / 2,
+                0,
+                gridY * gridSize + gridSize / 2,
+            )
+        }
+    }
+
     return (
         <Canvas shadowMap camera={{position:[8,5,0]}}>
             <OrbitControls maxPolarAngle={Math.PI * 0.5} />
@@ -14,6 +34,16 @@ export default function EditPage() {
             <directionalLight />
             <ambientLight />
             <gridHelper args={[levelSize, gridCount, 0x440000, 0x444444]} />
+            <mesh name="hidden-plane" rotation-x={-Math.PI / 2} onPointerMove={moveCursor}>
+                <planeBufferGeometry args={[levelSize,levelSize]}/>
+                <meshBasicMaterial visible={false}/>
+            </mesh>
+            <group ref={cursorRef} name="cursor-container">
+                <mesh name="cursor" rotation-x={-Math.PI / 2} position-y={0.01}>
+                    <planeBufferGeometry args={[1,1,3,3]} />
+                    <meshBasicMaterial color={0x000044} wireframe />
+                </mesh>
+            </group>
         </Canvas>
     )
 }
